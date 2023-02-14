4a3f9aa99229611317ad16d776df8b49cf7c1a70
ACER NITRO 5 <ACER NITRO 5@DESKTOP-P986EIO>
2023-02-13 21:10:11 -0500

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿
+class Ejercicio1
+{
+
+    static public void Main()
+    {
+        nombre();
+        numero();
+        dirreccion();
+        Email();
+        cliente();
+
+        void nombre()
+        {
+            Console.WriteLine("Introduce tu nombre completo: ");
+            string Nombre = Console.ReadLine();
+        }
+        void numero()
+
+        {
+            Console.WriteLine("Introduce tu numero telefonico: ");
+            double Numero = Convert.ToDouble(Console.ReadLine());
+
+        }
+        void dirreccion()
+        {
+            Console.WriteLine("Introduce tu dirreccion: ");
+            string Dirreccion = Console.ReadLine();
+        }
+        void Email()
+        {
+            Console.WriteLine("Introduce tu Email: ");
+            string Email = Console.ReadLine();
+        }
+        void cliente()
+        {
+            Console.WriteLine("Eres nuevo cliente: ");
+            string Cliente = Console.ReadLine();
+        }
+
+    }
+}
